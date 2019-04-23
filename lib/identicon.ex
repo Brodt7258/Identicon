f24bf8fe7fc28a6631840287f608ef02bc423e3a
Tim Dill <brodt7258@gmail.com>
2019-04-23 02:34:14 -0400

    save image to local folder

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -7,6 +7,11 @@ defmodule Identicon do
     |> filter_odd_squares
     |> build_pixel_map
     |> draw_image
+    |> save_image(input)
+  end
+
+  def save_image(image, input) do
+    File.write!("#{input}.png", image)
   end
 
   def draw_image(%Identicon.Image{color: color, pixel_map: pixel_map}) do
@@ -35,7 +40,7 @@ defmodule Identicon do
   end
 
   def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
-    Enum.filter grid, fn({code, _index}) ->
+    grid = Enum.filter grid, fn({code, _index}) ->
       rem(code, 2) == 0
     end
 
